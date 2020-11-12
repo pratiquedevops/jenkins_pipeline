dc9eb8de12bd8f37c30af1e17883ec94e09d1d27
pratiquedevops <70845555+pratiquedevops@users.noreply.github.com>
2020-11-12 18:08:37 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,23 @@
 
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+        
+        stage('Nana') {
+            steps {
+                echo 'Hello Nana'
+            }
+        }
+        stage('Moro') {
+            steps {
+                echo 'Hello Moro'
+            }
+        }
+    }
+}
